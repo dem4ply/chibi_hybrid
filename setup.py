1cfe3e359c55f8938cd46bba9ee513c7645e03ac
dem4ply <dem4ply@gmail.com>
2024-10-24 22:08:37 -0600

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/dem4ply/chibi_hybrid',
-    version='1.2.0',
+    version='1.2.1',
     zip_safe=False,
 )
